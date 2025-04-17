8a0692096bf3a61bcccce5c22a4a940559e96a5e
rizapranata <riza.ari@wgs-id.com>
2025-04-17 16:11:05 +0700

    init global colors constant

---
+++ app/constants/colors.ts
@@ -0,0 +1,23 @@
+// src/constants/colors.ts
+const Colors = {
+  primary: '#007bff',
+  secondary: '#6c757d',
+  success: '#28a745',
+  danger: '#dc3545',
+  warning: '#ffc107',
+  info: '#17a2b8',
+  light: '#f8f9fa',
+  dark: '#343a40',
+  white: '#ffffff',
+  black: '#000000',
+  gray: '#6e6e6e',
+
+  background: '#f2f2f2',
+  text: '#333333',
+
+  // Custom
+  todoCompleted: '#cce5ff',
+  todoPending: '#ffeeba',
+};
+
+export default Colors;
